89c580819596d7fb580de5eb2597014c46216611
Adam Wendelin <adwe@live.se>
2017-12-09 11:50:55 +0100

    cleanup

--- 9/9.py
+++ 9/9.py
@@ -7,22 +7,18 @@ is_garbage = False
 skip_next = False
 garbage_count = 0
 
-for i in range(len(data)):
-    c = data[i]
+for c in data:
     if not is_garbage:
         if c == '{':
             level += 1
             score += level
         elif c == '}':
             level -= 1
-        elif c == ',':
-            pass
         elif c == '<':
             is_garbage = True
     else:
         if skip_next:
             skip_next = False
-            continue
         elif c == '>':
             is_garbage = False
         elif c == '!':
